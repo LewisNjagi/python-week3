2124da0e5ea8830bf03fccff5978175d0fefce41
Lewis Njagi <lewismaybach@gmail.com>
2021-03-03 21:53:07 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_AQUA_URL")
 
 
 class DevConfig(Config):
